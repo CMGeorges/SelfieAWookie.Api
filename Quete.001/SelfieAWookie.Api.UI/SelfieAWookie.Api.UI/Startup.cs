e3be1c9f89fb55893395093b87621a5ec6c391b7
CMGeorges <61295187+CMGeorges@users.noreply.github.com>
2021-07-09 03:39:13 -0400

    :bulb:  Change to staging :building_construction:

--- Quete.001/SelfieAWookie.Api.UI/SelfieAWookie.Api.UI/Startup.cs
+++ Quete.001/SelfieAWookie.Api.UI/SelfieAWookie.Api.UI/Startup.cs
@@ -52,9 +52,10 @@ namespace SelfieAWookie.Api.UI
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "SelfieAWookie.Api.UI v1"));
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "SelfieAWookie.Api.UI v1"));
 
             app.UseHttpsRedirection();
 
